18d02273576ca4109d63c6dc7c856f997601c5ea
dzhyhun <danilsavc@gmail.com>
2023-10-22 12:29:48 +0300

    check functional

--- src/components/Header/ModalBar.jsx
+++ src/components/Header/ModalBar.jsx
@@ -4,7 +4,7 @@ import cancel from "../../Assets/Img/cancel.svg";
 
 const ModalBar = ({ active, setActive }) => {
   return (
-    <div className='w-screen h-screen fixed top-0 left-0 bg-neutral-800/50 z-10 grid grid-cols-6'>
+    <div className='w-screen fixed top-0 left-0 bg-neutral-800/50 z-10 grid grid-cols-6'>
       <div className='col-span-5 bg-white overflow-scroll'>
         Sit ut ea commodo id voluptate aliqua non. Aute mollit adipisicing consequat irure aute eu
         fugiat excepteur. Aliquip cupidatat sunt pariatur velit velit amet amet adipisicing proident

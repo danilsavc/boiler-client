f6a765ba4e51779509b1c99475e4e7795a286a43
dzhyhun <danilsavc@gmail.com>
2023-10-22 12:18:09 +0300

    check functional

--- src/components/Header/ModalBar.jsx
+++ src/components/Header/ModalBar.jsx
@@ -5,7 +5,77 @@ import cancel from "../../Assets/Img/cancel.svg";
 const ModalBar = ({ active, setActive }) => {
   return (
     <div className='w-screen h-screen overflow-y-hidden fixed top-0 left-0 bg-neutral-800/50 z-10 grid grid-cols-6'>
-      <div className='h-screen col-span-5 bg-white'></div>
+      <div className=' col-span-5 bg-white overflow-scroll'>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+        <p>asd</p>
+      </div>
       <button className='w-11 h-11 justify-self-center rounded-md bg-orange-400 flex justify-center items-center mt-4 mr-1'>
         <img src={cancel} alt='cancel' className='w-6 h-6' />
       </button>
